a3ac2393fa216606176f261c1d56021dd4442546
ratu36 <161916786+ratu36@users.noreply.github.com>
2024-03-02 12:48:08 +0700

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <!-- <h1>Hello, world!</h1> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
+<link rel="apple-touch-icon" sizes="57x57" href="home/img/favicon/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="home/img/favicon/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="home/img/favicon/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="home/img/favicon/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="home/img/favicon/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="home/img/favicon/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="home/img/favicon/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="home/img/favicon/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="home/img/favicon/apple-touch-icon-180x180.png">
+<link rel="icon" type="home/image/png" href="home/img/favicon/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="home/image/png" href="home/img/favicon/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="home/image/png" href="home/img/favicon/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="home/image/png" href="home/img/favicon/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="home/img/favicon/manifest.json">
+<link rel="shortcut icon" href="home/img/favicon/favicon.ico">
+<meta name="msapplication-TileColor" content="#663fb5">
+<meta name="msapplication-TileImage" content="home/img/favicon/mstile-144x144.png">
+<meta name="msapplication-config" content="home/img/favicon/browserconfig.xml">
+<meta name="theme-color" content="#663fb5">
+
+
+<link rel="stylesheet" type="text/css" href="./asset/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="./asset/css/dataTables.bootstrap.min.css"/>
+<link rel="stylesheet" type="text/css" href="./asset/sweetalert/dist/sweetalert.css">
+<script src="./asset/js/jquery.min.js" ></script>
+<script src="./asset/js/bootstrap.min.js" ></script>
+<script src="./asset/js/datatables.min.js" ></script>
+<script src="./asset/js/dataTables.bootstrap.min.js" ></script>
+<script type="text/javascript" src="./asset/sweetalert/dist/sweetalert.min.js"></script>
